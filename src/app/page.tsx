5dd8c3c6bba6484d1efa4e987f316b773b79a509
Solaiman.ovi <solaiman.arisaftech@gmail.com>
2023-10-30 17:38:48 +0600

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from '@/components/navbar/navbar'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center ">
+      <Navbar/>
+      
+    </main>
+  )
+}
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,85 @@
+"use client";
+import React from "react";
+import { useState } from "react";
+import Link from "next/link";
+import { User2, ShoppingCart, Heart, Menu } from "lucide-react";
+
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <nav className="w-full bg-white h-14 flex justify-between items-center shadow-lg px-4 mx-auto md:px-64 ">
+      
+        <div className="text-NeonPink font-extrabold text-4xl">
+          Shoe
+          <span className="text-purple">
+            <u>s</u>
+          </span>
+        </div>
+        <div className="md:flex gap-10 hidden ">
+          <div className="hover:text-NeonPink font-medium">Home</div>
+          <div className="hover:text-NeonPink font-medium">Products</div>
+          <div className="hover:text-NeonPink font-medium">About</div>
+          <div className="hover:text-NeonPink font-medium">Review</div>
+          <div className="hover:text-NeonPink font-medium">Services</div>
+        </div>
+        <div className="hidden md:flex gap-2">
+          <Heart className="stroke-none fill-black hover:fill-NeonPink" />
+          <ShoppingCart className="stroke-black stroke-2 fill-black hover:fill-NeonPink hover:stroke-NeonPink" />
+          <User2 className="stroke-white fill-black hover:fill-NeonPink" />
+        </div>
+      
+      <div className="md:hidden  flex items-center">
+        <button
+          className="outline-none mobile-menu-button"
+          onClick={toggleMobileMenu}
+        >
+          <Menu
+            className={`w-6 h-6 text-gray-500 fill-none stroke-current ${
+              isMobileMenuOpen ? "hover:text-primary" : " "
+            }`}
+          />
+        </button>
+      </div>
+      {/* <div
+        className={`md:hidden mobile-menu mt-64 ${isMobileMenuOpen ? "" : "hidden"}`}
+      >
+        <ul className="flex flex-col gap-4 border-2 w-full ">
+          <li className="active   ">
+            <Link href="index.html" className=" hover:stroke-NeonPink">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="#services" className="block hover:stroke-NeonPink">
+              Products
+            </Link>
+          </li>
+
+          <li className="hover:stroke-NeonPink">
+            <Link href="#contact" className="block  hover:stroke-NeonPink">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="" className="block  hover:stroke-NeonPink">
+              Review
+            </Link>
+          </li>
+          <li>
+            <Link href="" className="block  hover:stroke-NeonPink">
+              Services
+            </Link>
+          </li>
+        </ul>
+      </div> */}
+      
+    </nav>
+  );
+};
+
+export default Navbar;
