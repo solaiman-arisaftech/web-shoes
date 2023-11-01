3387c95cf8642f6de45bcb44303b3d30693a7854
unknown <msovi480@gmail.com>
2023-11-01 13:51:01 +0600

    Feat: product card

--- src/components/products/productCard.tsx
+++ src/components/products/productCard.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import Image from "next/image";
 
-import img1 from "../../../public/resources/shoes5.png";
+import img1 from "../../../public/resources/shoes3.png";
 
 const ProductCard = () => {
   return (
-    <div className="w-full p-4 border-2 flex flex-col items-center ">
+    <div className="w-full  border-2 flex flex-col items-center rounded-lg shadow-purple shadow-md bg-white">
+
       <div className="w-1/2 p-4">
         <Image src={img1} alt=""/>
       </div>
-      <div className="w-1/2 flex flex-col">
-        <div>NIKE</div>
-        <div className="">Lorem ipsum dolor sit, amet consectetur adipisicing elit</div>\
-        <div>$100.99</div>
+      <div className=" flex flex-col p-4 gap-2">
+        <div className="text-center text-3xl font-bold">NIKE</div>
+        <div className="text-center text-slate-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit</div>
+        <div className="text-center font-bold text-lg">$100.99</div>
         <div>
          
         </div>
-        <div>Add to cart</div>
+        <button className="w-1/2 transition duration-200 ease-in-out  rounded-sm self-center text-center text-white p-1 bg-gradient-to-r from-NeonPink to-purple">Add to cart</button>
       </div>
     </div>
   );
